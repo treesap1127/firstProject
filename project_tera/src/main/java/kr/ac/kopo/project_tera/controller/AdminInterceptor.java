9340512715e5a6c5d7f5d4472954e55ed5e454cb
Taesung Lee <xotjddl1127@gmail.com>
2022-04-01 13:57:06 +0900

    pom_change

--- project_tera/src/main/java/kr/ac/kopo/project_tera/controller/AdminInterceptor.java
+++ project_tera/src/main/java/kr/ac/kopo/project_tera/controller/AdminInterceptor.java
@@ -4,15 +4,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import kr.ac.kopo.project_tera.model.Partner;
-
+@Controller
 public class AdminInterceptor extends HandlerInterceptorAdapter {
-
+	
+//	@Autowired
+//	AdminService service;
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		
 		System.out.println("AdminInterceptor");
 		HttpSession session =request.getSession();
 		Partner partner=(Partner) session.getAttribute("partner");
